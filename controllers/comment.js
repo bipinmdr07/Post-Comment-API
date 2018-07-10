62c83319375d526545bb507309c1e7c4b105abbb
Bipin Manandhar <kubipin07@gmail.com>
2018-07-10 06:29:05 +0545

    Add basic implementation of post and comment controller without filter

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,24 +1,40 @@
 const router = require('express').Router();
-const database = require('../db');
+const commentServices = require('../services/commentServices');
 
 // getting all comments
-router.get('/', (request, response) => {
-
+router.get('/', async (request, response) => {
+  try {
+    response.status(200).json(await commentServices.getAllComments());
+  } catch (err) {
+    response.status(500).json({ err });
+  }
 });
 
 // adding new comment
-router.post('/', (request, response) => {
-
+router.post('/', async (request, response) => {
+  try {
+    response.status(201).json(await commentServices.addNewComment(request.body));
+  } catch (err) {
+    response.status(400).json({ err });
+  }
 });
 
 // updating particular comment
-router.put('/:id', (request, response) => {
-
+router.put('/:id', async (request, response) => {
+  try {
+    response.status(200).json(await commentServices.updateComment(request.params.id));
+  } catch (err) {
+    response.status(404).json({ err });
+  }
 });
 
 // deleting particular comment
-router.delete('/:id', (request, response) => {
-
+router.delete('/:id', async (request, response) => {
+  try {
+    response.status(200).json(await commentServices.deleteComment(request.params.id));
+  } catch (err) {
+    response.status(304).json({ err });
+  }
 });
 
 module.exports = router;
--- controllers/post.js
+++ controllers/post.js
@@ -1,33 +1,49 @@
 const router = require('express').Router();
-const database = require('../db');
+const postServices = require('../services/postServices');
 
 // getting all post
 router.get('/', async (request, response) => {
   try {
-    
+    response.status(200).json(await postServices.getAllPosts());
   } catch (err) {
-
+    responst.status(404).json({ err });
   }
 });
 
 // adding new post
-router.post('/', (request, response) => {
-
+router.post('/', async (request, response) => {
+  try {
+    response.status(201).json(await postServices.addNewPost(request.body));
+  } catch (err) {
+    response.status(400).json({ err });
+  }
 });
 
 // getting particular posts
-router.get('/:id', (request, response) => {
-
+router.get('/:id', async (request, response) => {
+  try {
+    response.status(200).json(await postServices.getPost(request.params.id));
+  } catch (err) {
+    responsse.status(404).json({ err });
+  }
 });
 
 // updating particular post
-router.put('/:id', (request, response) => {
-
+router.put('/:id', async (request, response) => {
+  try {
+    response.status(200).json(await postServices.updatePost(request.params.id));
+  } catch (err) {
+    response.status(404).json({ err });
+  }
 });
 
 // deleting particular post
-router.delete(':/id', (request, response) => {
-
+router.delete(':/id', async (request, response) => {
+  try {
+    response.status(200).json(await postServices.deletePost(request.params.id));
+  } catch (err) {
+    response.status(304).json({ err });
+  }
 });
 
 module.exports = router;
