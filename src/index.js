f25e795572a6c05d22a411d05b494ae9523e0f78
Bipin Manandhar <kubipin07@gmail.com>
2018-07-15 23:20:25 +0545

    Setup CORS to include Authorization as header

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+this.app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin, profilerefid(whatever header you need)");
   next();
 });
 
